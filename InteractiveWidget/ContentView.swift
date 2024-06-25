e2e1c1189e6c1ec64594f9311a3ba004b390a91b
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2024-06-25 18:28:20 +0300

    Initial Commit

---
+++ InteractiveWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InteractiveWidget
+//
+//  Created by Егор Аблогин on 25.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InteractiveWidget/InteractiveWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveWidgetApp.swift
+//  InteractiveWidget
+//
+//  Created by Егор Аблогин on 25.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
