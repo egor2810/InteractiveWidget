a9135605f5c855e019053cdd18cb933f590554f7
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2024-06-25 19:31:03 +0300

    create interactive widget

---
+++ InterWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  InterWidget
+//
+//  Created by Егор Аблогин on 25.06.2024.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ InterWidget/InterWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  InterWidgetBundle.swift
+//  InterWidget
+//
+//  Created by Егор Аблогин on 25.06.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct InterWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        InterWidget()
+        InterWidgetLiveActivity()
+    }
+}
